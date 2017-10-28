97a0a47fbc937707508fb6696ce7ecc57cedc4a6
Harry Hum <harryhum22@hotmail.com>
2017-10-28 14:55:32 -0400

    Added countdown display and buttons

--- js/script.js
+++ js/script.js
@@ -1,75 +1,126 @@
+var mode = 'work';
+var time;
+
 $(document).ready(function() {
   setWork();
   setRest();
-//  timer();
-//  start();
-//  pause();
-//  stop();
-//  reset();
+  clockDisplay(mode);
+  start();
 });
+
+function switchMode(startTime) {
+    clearInterval(startTime);
+    if (mode == 'rest') {
+      mode = 'work';
+      fromTime = $('#work').text() * 1000 * 60;
+    } else {
+      mode = 'rest';
+      fromTime = $('#rest').text() * 1000 * 60;
+    }
+    timer(mode);
+    countdown(fromTime);
+};
+
+/*
+* Starts the countdown of the clock.
+* @param {number} [fromTime] The number to start counting down from.
+*/
+function countdown(fromTime) {
+  console.log(fromTime);
+  var startTime = setInterval(function() {
+    fromTime = fromTime - 1000;
+    var minutes = Math.floor((fromTime % (1000 * 60 *60)) / (1000 * 60));
+    var seconds = Math.floor((fromTime % (1000* 60)) / 1000);
+    if (seconds < 10) {
+      timeDisplay = minutes.toString() + ':0' + seconds.toString();
+    } else {
+      var timeDisplay = minutes.toString() + ':' + seconds.toString();
+    };
+    $('#countdown').text(timeDisplay);
+    if (fromTime == 0) {
+      switchMode(startTime);
+    }
+  }, 1000);
+
+  $('#pause').on('click', function() {
+    clearInterval(startTime);
+  });
+  $('#stop').on('click', function() {
+    clearInterval(startTime);
+    clockDisplay(mode);
+  });
+  $('#reset').on('click', function() {
+    clearInterval(startTime);
+    $('#work').text('25');
+    $('#rest').text('5');
+    clockDisplay(mode);
+  });
+};
+
+function modeChooser() {
+  if (mode == 'work') {
+    time = $('#work').text() * 1000 * 60;
+  } else {
+    time = $('#rest').text() * 1000 * 60;
+  }
+};
+
+function clockDisplay(mode) {
+  modeChooser();
+  var minutes = Math.floor((time % (1000 * 60 *60)) / (1000 * 60));
+  var seconds = 0;
+  if (seconds < 10) {
+    timeDisplay = minutes.toString() + ':0' + seconds.toString();
+  } else {
+    var timeDisplay = minutes.toString() + ':' + seconds.toString();
+  };
+  $('#countdown').text(timeDisplay);
+};
+
+//Starts the countdown using the time currently displayed on the clock.
+function start() {
+  $('#start').on('click', function () {
+    var min = $('#countdown').text().substr(0, $('#countdown').text().indexOf(':')) * 1000 * 60;
+    var sec = $('#countdown').text().substr(($('#countdown').text().length - 2), $('#countdown').text().length) * 1000;
+    var currentTime = min + sec;
+    countdown(currentTime);
+  });
+};
 
 function setWork() {
   var workTime = parseInt($('#work').text());
   $('#work').text(workTime.toString());
-  $('.upWork').on('click', function() {
+  $('#upWork').on('click', function() {
     workTime = workTime + 1;
     $('#work').text(workTime.toString());
+    clockDisplay(mode);;
   });
-  $('.downWork').on('click', function() {
+  $('#downWork').on('click', function() {
     if (workTime > 0) {
       workTime = workTime - 1;
     } else {
       workTime = 0
     };
     $('#work').text(workTime.toString());
+    clockDisplay(mode);;
   });
 };
 
 function setRest() {
   var restTime = parseInt($('#rest').text());
   $('#rest').text(restTime.toString());
-  $('.upRest').on('click', function() {
+  $('#upRest').on('click', function() {
     restTime = restTime + 1;
     $('#rest').text(restTime.toString());
+    clockDisplay(mode);;
   });
-  $('.downRest').on('click', function() {
+  $('#downRest').on('click', function() {
     if (restTime > 0) {
       restTime = restTime - 1;
     } else {
       restTime = 0
     };
     $('#rest').text(restTime.toString());
+    clockDisplay(mode);
   });
 };
-
-/* failed attempt to use DRY methodology
-function iterateUp(id) {
-  $('.up').on('click', function() {
-    var time = parseInt($(this).closest('p'));
-    time = time + 1;
-    $('#' + id).text(time.toString());
-  });
-};
-
-function iterateDown(id) {
-  $('.down').on('click', function() {
-    var time = parseInt($(this).closest('p'));
-    if (time > 0) {
-      time = time - 1;
-    } else {
-      time = 0;
-    };
-    $('#' + id).text(time.toString());
-  });
-};
-
-function setWork() {
-  iterateUp('work');
-  iterateDown('work');
-};
-
-function setRest() {
-  iterateUp('rest');
-  iterateDown('rest');
-}
-*/
